4df61ebe8f81e7396a52081f9d5b1b712305162b
britinimalloy <britini.malloy@outlook.com>
2017-06-19 08:42:37 -0700

    oh crap

--- app.js
+++ app.js
@@ -2,93 +2,84 @@
 
 console.log('hello world');
 
-/* alert('Want to play a game? It\'ll be fun, I PROMISE!');
+alert('Want to play a game? It\'ll be fun, I PROMISE!');
 alert('I\'m going to ask you a series of questions, you MUST answer correctly. Your life depends on it!');
 alert('Just kidding...');
-alert('Or AM I???'); */
+alert('Or AM I???');
 
-var user = prompt('First question: What is your name?').toUpperCase();
-console.log(user);
-alert('Thank you for playing with me, ' + user);
-
-/*
-var question2 = prompt('Next question: ' + user + ', Do you think I, the wonderful and magnificent Britini, lift weights? Answer with a yes or no. OR ELSE!').toLowerCase();
-console.log(question2);
-if (question2 === 'yes' || question2 === 'y') {
-  alert(user + ', you are correct! Congratulations, you get to live for another round.');
-} else if (question2 === 'no' || question2 === 'n') {
-  alert(user + ', you are incorrect! Watch it, too many wrong answers and you may not live through this game...');
-} else {
-  alert(user + ', you are terrible at following instructions. I will take mercy for now, DON\'T SCREW UP AGAIN!!');
-}
-
-var question3 = prompt('Second yes or no answered question, ' + user + ': do you think I do cardio?').toLowerCase();
-console.log(question3);
-if (question3 === 'no' || question3 === 'n') {
-  alert(user + ', you are correct! Congratulations, you get to live for the 3rd round. I don\'t like cardio');
-} else if (question3 === 'yes' || question3 === 'y') {
-  alert(user + ', you are incorrect! Watch it, too many wrong answers and you may not live through this game...just like I don\'t when forced to do cardio');
-} else {
-  alert(user + ', you are terrible at following instructions. I have told you, I may not take mercy on you, DON\'T SCREW UP AGAIN!!');
-}
-
-var question4 = prompt(user + ', do you think I can back squat more than my body weight?').toLowerCase();
-console.log(question4);
-if (question4 === 'yes' || question4 === 'y') {
-  alert(user + ', you are correct! Congratulations, you get to live for the 4th round.');
-} else if (question4 === 'no' || question4 === 'n') {
-  alert(user + ', you are incorrect! Watch it, I don\'t like to be insulted!');
-} else {
-  alert(user + ', you are terrible at following instructions. I am getting irritated with you...');
-}
-
-var question5 = prompt(user + ', are you under the illusion that I like to run?').toLowerCase();
-console.log(question5);
-if (question5 === 'no' || question5 === 'n') {
-  alert(user + ', you are correct! Congratulations, you get to live for the final round because you listen.');
-} else if (question5 === 'yes' || question5 === 'y') {
-  alert(user + ', don\'t you listen?? I HATE CARDIO!!!');
-} else {
-  alert(user + ', you are horrendous at following instructions. I am just about done with your lack of listening!');
-}
-
-var question6 = prompt(user + ', here\'s your final question: is core day my favorite day?').toLowerCase();
-console.log(question6);
-if (question6 === 'yes' || question6 === 'y') {
-  alert(user + ', you are correct! Congratulations, you made it through the game alive!');
-} else if (question6 === 'no' || question6 === 'n') {
-  alert(user + ', you are incorrect! I\'m not sure what to do with you...');
-} else {
-  alert(user + ', this is the end of the game! PREPARE TO DIE!!');
-}
-*/
-
-var question7 = NaN;
-question7 = parseInt(prompt('Next question: ' + user + ', can you guess my favorite number?'));
-console.log(question7);
-/*alert('hello');
-console.log('hello');
-console.log(parseInt(question7));
-var count = 1;
-console.log(count);
-var input = parseInt(question7);
-console.log(input);*/
-
-for (var ii = 1; ii < 5; ii++) {
-  if (question7 === 5) {
-    alert('Yay! You got it correct!');
-    console.log('Yay it is correct!');
-  }/* else */
-  if (ii >= 5) {
-    alert(user + ', you have guessed incorrectly 4 times. You are done.');
-  }
-  if (isNaN(question7)) {
-    //
-  } else if (question7 > 5) {
-    //
-  } else if(question7 < 5) {
-    //
-  }
-}
+// var user = prompt('First question: What is your name?').toUpperCase();
+// console.log(user);
+// alert('Thank you for playing with me, ' + user);
+//
+// var question2 = prompt('Next question: ' + user + ', Do you think I, the wonderful and magnificent Britini, lift weights? Answer with a yes or no. OR ELSE!').toLowerCase();
+// console.log(question2);
+// if (question2 === 'yes' || question2 === 'y') {
+//   alert(user + ', you are correct! Congratulations, you get to live for another round.');
+// } else if (question2 === 'no' || question2 === 'n') {
+//   alert(user + ', you are incorrect! Watch it, too many wrong answers and you may not live through this game...');
+// } else {
+//   alert(user + ', you are terrible at following instructions. I will take mercy for now, DON\'T SCREW UP AGAIN!!');
+// }
+//
+// var question3 = prompt('Second yes or no answered question, ' + user + ': do you think I do cardio?').toLowerCase();
+// console.log(question3);
+// if (question3 === 'no' || question3 === 'n') {
+//   alert(user + ', you are correct! Congratulations, you get to live for the 3rd round. I don\'t like cardio');
+// } else if (question3 === 'yes' || question3 === 'y') {
+//   alert(user + ', you are incorrect! Watch it, too many wrong answers and you may not live through this game...just like I don\'t when forced to do cardio');
+// } else {
+//   alert(user + ', you are terrible at following instructions. I have told you, I may not take mercy on you, DON\'T SCREW UP AGAIN!!');
+// }
+//
+// var question4 = prompt(user + ', do you think I can back squat more than my body weight?').toLowerCase();
+// console.log(question4);
+// if (question4 === 'yes' || question4 === 'y') {
+//   alert(user + ', you are correct! Congratulations, you get to live for the 4th round.');
+// } else if (question4 === 'no' || question4 === 'n') {
+//   alert(user + ', you are incorrect! Watch it, I don\'t like to be insulted!');
+// } else {
+//   alert(user + ', you are terrible at following instructions. I am getting irritated with you...');
+// }
+//
+// var question5 = prompt(user + ', are you under the illusion that I like to run?').toLowerCase();
+// console.log(question5);
+// if (question5 === 'no' || question5 === 'n') {
+//   alert(user + ', you are correct! Congratulations, you get to live for the final round because you listen.');
+// } else if (question5 === 'yes' || question5 === 'y') {
+//   alert(user + ', don\'t you listen?? I HATE CARDIO!!!');
+// } else {
+//   alert(user + ', you are horrendous at following instructions. I am just about done with your lack of listening!');
+// }
+//
+// var question6 = prompt(user + ', here\'s your final question: is core day my favorite day?').toLowerCase();
+// console.log(question6);
+// if (question6 === 'yes' || question6 === 'y') {
+//   alert(user + ', you are correct! Congratulations, you made it through the game alive!');
+// } else if (question6 === 'no' || question6 === 'n') {
+//   alert(user + ', you are incorrect! I\'m not sure what to do with you...');
+// } else {
+//   alert(user + ', this is the end of the game! PREPARE TO DIE!!');
+// }
+//
+// var question7 = NaN;
+// question7 = parseInt(prompt('Next question: ' + user + ', can you guess my favorite number?'));
+// console.log(question7);
+//
+// for (var ii = 1; ii < 5; ii++) {
+//   if (question7 === 5) {
+//     alert('Yay! You got it correct!');
+//     console.log('Yay it is correct!');
+//   }/* else */
+//   if (ii >= 5) {
+//     alert(user + ', you have guessed incorrectly 4 times. You are done.');
+//   }
+//   if (isNaN(question7)) {
+//     //
+//   } else if (question7 > 5) {
+//     //
+//   } else if(question7 < 5) {
+//     //
+//   }
+// }
 
 //question8 = ;
